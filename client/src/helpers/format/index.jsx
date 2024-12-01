063fe9e8fb6003e0593ac3a59182675c4caae804
nguyenkhoa02 <nguyenkhoaht002@gmail.com>
2024-12-02 01:53:15 +0700

    fix -n

--- client/src/helpers/format/index.jsx
+++ client/src/helpers/format/index.jsx
@@ -5,15 +5,18 @@ export const formatDate = (dateString) => {
   const month = String(dateString.getMonth() + 1).padStart(2, '0');
   const day = String(dateString.getDate()).padStart(2, '0');
   return `${year}-${month}-${day}`;
+
 };
 
 export const formatDateDMY = (dateString) => {
   if (!dateString) return '';
-  dateString = new Date(dateString);
-  const year = dateString.getFullYear();
-  const month = String(dateString.getMonth() + 1).padStart(2, '0');
-  const day = String(dateString.getDate()).padStart(2, '0');
-  return `${day}-${month}-${year}`;
+  // dateString = new Date(dateString);
+  // const year = dateString.getFullYear();
+  // const month = String(dateString.getMonth() + 1).padStart(2, '0');
+  // const day = String(dateString.getDate()).padStart(2, '0');
+  // return `${day}-${month}-${year}`;
+  return new Date(dateString).toLocaleDateString();
+
 };
 
 export const formatCurrency = (price) => {
